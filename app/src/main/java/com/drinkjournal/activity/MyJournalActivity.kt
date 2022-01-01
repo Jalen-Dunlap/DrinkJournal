20450d4b90dc9d878de278e017f8752b971e63bd
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-12-31 17:16:57 -0700

    Create xml views and related .kt activity controllers

---
+++ app/src/main/java/com/drinkjournal/activity/MyJournalActivity.kt
@@ -0,0 +1,12 @@
+package com.drinkjournal.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drinkjournal.R
+
+class MyJournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_journal)
+    }
+}
