0c317117ea14c230ac0eb7aa4f928e6a020099ce
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2022-01-10 08:45:34 -0700

    removed testBeer function and moved navigation to xml

--- app/src/main/java/com/drinkjournal/activity/MainActivity.kt
+++ app/src/main/java/com/drinkjournal/activity/MainActivity.kt
@@ -1,54 +1,33 @@
 package com.drinkjournal.activity
 
 import android.content.Intent
-import android.media.Rating
-import android.media.Rating.RATING_5_STARS
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
-import com.drinkjournal.dataClasses.BeerData
+import com.drinkjournal.db.DBHelper
 import com.drinkjournal.drinkjournal.R
 
 class MainActivity : AppCompatActivity() {
+
+    var myJournalDB = DBHelper(this)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        toNewDrink()
-
-        toMyJournal()
-
-        toSettings()
     }
 
-    fun testBeer(){
-        var beerData = BeerData("Alaskan IPA","IPA","Alaskan",
-                            "Juneau, AK",7, Rating.newStarRating(RATING_5_STARS,4.2f),
-                        "Fruity with a full body")
-        println(beerData.beerStyle)
+    fun toNewDrink(view: View){
+        startActivity(Intent(this, NewDrinkActivity::class.java))
     }
 
-    private fun toNewDrink(){
-        val newDrink = findViewById<Button>(R.id.newDrinkButton)
-        newDrink.setOnClickListener(View.OnClickListener {
-            startActivity(Intent(this, NewDrinkActivity::class.java))
-        })
+    fun toMyJournal(view: View){
+        startActivity(Intent(this,MyJournalActivity::class.java))
     }
 
-    private fun toMyJournal(){
-        val myJournal = findViewById<Button>(R.id.myJournalButton)
-        myJournal.setOnClickListener(View.OnClickListener {
-            startActivity(Intent(this,MyJournalActivity::class.java))
-        })
-    }
-
-    private fun toSettings(){
-        val settingsButton = findViewById<Button>(R.id.settingsButton)
-        settingsButton.setOnClickListener(View.OnClickListener {
-            Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
-            //startActivity(Intent(this, SettingsActivity::class.java))
-        })
+    fun toSettings(view: View){
+        Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
     }
 }
+
