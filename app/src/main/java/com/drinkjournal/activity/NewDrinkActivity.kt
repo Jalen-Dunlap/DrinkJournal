347547e585e79497e46233f12709ce7bf89fc338
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2022-01-10 16:26:55 -0700

    fixed crashing error due to improper new drink creation

--- app/src/main/java/com/drinkjournal/activity/NewDrinkActivity.kt
+++ app/src/main/java/com/drinkjournal/activity/NewDrinkActivity.kt
@@ -2,6 +2,7 @@ package com.drinkjournal.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
@@ -9,6 +10,9 @@ import android.widget.Toast
 import com.drinkjournal.dataClasses.DrinkData
 import com.drinkjournal.db.DBHelper
 import com.drinkjournal.drinkjournal.R
+import java.lang.Exception
+import java.lang.NumberFormatException
+import kotlin.math.roundToInt
 
 class NewDrinkActivity : AppCompatActivity(){
 
@@ -27,26 +31,40 @@ class NewDrinkActivity : AppCompatActivity(){
     private fun addDrinkButton(){
         val addButton = findViewById<Button>(R.id.addButton)
         addButton.setOnClickListener{
+            var alcoholPercent = 0
             val name = findViewById<EditText>(R.id.drinkName).text.toString()
             val type = findViewById<EditText>(R.id.drinkType).text.toString()
             val specs = findViewById<EditText>(R.id.drinkSpecs).text.toString()
-            val alcoholPercent = findViewById<EditText>(R.id.alcoholPercent).text.toString().toInt()
+            val alcoholPercentString = findViewById<EditText>(R.id.alcoholPercent).text.toString()
             val maker = findViewById<EditText>(R.id.drinkMaker).text.toString()
             val origin = findViewById<EditText>(R.id.drinkOrigin).text.toString()
             val description = findViewById<EditText>(R.id.drinkDescription).text.toString()
             val rating = findViewById<RatingBar>(R.id.ratingBar).rating
-        
-            newDrink = DrinkData(name,type,specs,alcoholPercent,maker,origin,description,rating)
 
-            // TODO: 1/10/2022 Create null exception for adding to DB
+            try {
+                alcoholPercent = alcoholPercentString.toFloat().toInt()
+            }catch (e: NumberFormatException){
+                Log.d("CATCH","Caught numberformat exception")
+                toastMessage("Alcohol Percentage needs to be fixed")
+            }catch (e: Exception){
+            }
 
-            val drinkSuccess = myJournalDB.addDrink(newDrink)
+            Log.d("MyActivity","DEBUG :::::::name:$name \n a%:$alcoholPercent \n rat:$rating")
+            if (name != "" && type != "" && specs != "" && alcoholPercent >= 0
+                && maker != "" && origin != "" && description != "" && rating >= 0) {
+                newDrink =
+                    DrinkData(name, type, specs, alcoholPercent, maker, origin, description, rating)
 
-            if (drinkSuccess > -1){
-                toastMessage("Added new drink to the journal")
-                finish()
+                val drinkSuccess = myJournalDB.addDrink(newDrink)
+
+                if (drinkSuccess > -1) {
+                    toastMessage("Added new drink to the journal")
+                    finish()
+                } else {
+                    toastMessage("Failed to add new Drink")
+                }
             }else{
-                toastMessage("Failed to add new Drink")
+                toastMessage("You need to fill in all of the fields for the drink")
             }
         }
     }
