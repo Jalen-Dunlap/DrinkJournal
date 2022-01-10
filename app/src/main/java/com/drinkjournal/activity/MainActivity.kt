1eb7f176333ac5a62e1b679138417c35fea8594f
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2022-01-10 08:55:08 -0700

    fixed navigation for main

--- app/src/main/java/com/drinkjournal/activity/MainActivity.kt
+++ app/src/main/java/com/drinkjournal/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.drinkjournal.db.DBHelper
 import com.drinkjournal.drinkjournal.R
@@ -12,22 +13,46 @@ class MainActivity : AppCompatActivity() {
 
     var myJournalDB = DBHelper(this)
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toNewDrink()
+
+        toMyJournal()
+
+        toSettings()
     }
 
-    fun toNewDrink(view: View){
-        startActivity(Intent(this, NewDrinkActivity::class.java))
+    private fun toNewDrink(){
+        val newDrink = findViewById<Button>(R.id.newDrinkButton)
+        newDrink.setOnClickListener {
+            View.OnClickListener {
+                startActivity(Intent(this, NewDrinkActivity::class.java))
+            }
+        }
+
     }
 
-    fun toMyJournal(view: View){
-        startActivity(Intent(this,MyJournalActivity::class.java))
+    private fun toMyJournal(){
+        val myJournal = findViewById<Button>(R.id.myJournalButton)
+        myJournal.setOnClickListener{View.OnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MyJournalActivity::class.java
+                )
+            )
+        }}
     }
 
-    fun toSettings(view: View){
-        Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
+    private fun toSettings(){
+        val settingsButton = findViewById<Button>(R.id.settingsButton)
+        settingsButton.setOnClickListener{
+            View.OnClickListener {
+                Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
+                //startActivity(Intent(this, SettingsActivity::class.java))
+            }
+        }
     }
 }
-
