8e24f46c07b15a5d8e717e7c210a1d4194a53c7c
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2022-01-10 11:47:24 -0700

    fixed navigation error

--- app/src/main/java/com/drinkjournal/activity/MainActivity.kt
+++ app/src/main/java/com/drinkjournal/activity/MainActivity.kt
@@ -11,8 +11,6 @@ import com.drinkjournal.drinkjournal.R
 
 class MainActivity : AppCompatActivity() {
 
-    var myJournalDB = DBHelper(this)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,32 +25,28 @@ class MainActivity : AppCompatActivity() {
     private fun toNewDrink(){
         val newDrink = findViewById<Button>(R.id.newDrinkButton)
         newDrink.setOnClickListener {
-            View.OnClickListener {
                 startActivity(Intent(this, NewDrinkActivity::class.java))
-            }
         }
 
     }
 
     private fun toMyJournal(){
         val myJournal = findViewById<Button>(R.id.myJournalButton)
-        myJournal.setOnClickListener{View.OnClickListener {
+        myJournal.setOnClickListener{
             startActivity(
                 Intent(
                     this,
                     MyJournalActivity::class.java
                 )
             )
-        }}
+        }
     }
 
     private fun toSettings(){
         val settingsButton = findViewById<Button>(R.id.settingsButton)
         settingsButton.setOnClickListener{
-            View.OnClickListener {
-                Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
-                //startActivity(Intent(this, SettingsActivity::class.java))
-            }
+            Toast.makeText(applicationContext,"Not currently available", Toast.LENGTH_SHORT).show()
+            //startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
